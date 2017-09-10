481f20380ab7c21e83d63c4c4f3234e1c77fd003
Juan José Granados <juanjogramo@gmail.com>
2017-09-10 18:15:56 -0500

    Create moroso Controller on WebApiRest

---
+++ BackEnd/WebApiRest/Controllers/morosoController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessEntities;
+using BussinessRules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiRest.Controllers
+{
+    public class morosoController : ApiController
+    {
+        [HttpPost]
+        [Route("Morosos")]
+        public HttpResponseMessage P0005SHPR_USUA(BESHMC_MORO oBe)
+        {
+            var oBr = new BRSHMC_MORO();
+            try
+            {
+                var oList = oBr.P0007SHPR_MORO_LIST(oBe);
+                return Request.CreateResponse(HttpStatusCode.OK, oList);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
