0aeff2c187d097089bb8c3a3f70eadb61cee76e2
Juan José Granados <juanjogramo@gmail.com>
2017-09-10 18:18:49 -0500

    Create defaulter model

---
+++ BackEnd/WebApiRest/Controllers/BESHMC_MORO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class BESHMC_MORO
+    {
+        public int COD_MORO { get; set; }
+        public int COD_AGEN { get; set; }
+        public string COD_UBIG { get; set; }
+        public string ALF_MORO { get; set; }
+        public string ALF_DNII { get; set; }
+        public string ALF_DIRE { get; set; }
+        public string ALF_DESC_DEUD { get; set; }
+        public string ALF_DEPA { get; set; }
+        public string ALF_PROV { get; set; }
+        public string ALF_DIST { get; set; }
+        public string COD_USUA_CREA { get; set; }
+        public string COD_USUA_MODI { get; set; }
+        public int NUM_ACCI { get; set; }
+    }
+}
