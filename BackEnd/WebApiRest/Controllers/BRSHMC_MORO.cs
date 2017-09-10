b78c6379b518ef3fcdb4b7725d23f2676fd5f4e7
Juan José Granados <juanjogramo@gmail.com>
2017-09-10 18:18:12 -0500

    Get colaborators list controller

---
+++ BackEnd/WebApiRest/Controllers/BRSHMC_MORO.cs
@@ -0,0 +1,35 @@
+﻿using BusinessEntities;
+using DataAccess;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResultSetMappers;
+
+namespace BussinessRules
+{
+    public class BRSHMC_MORO
+    {
+        private DASHMC_MORO oDa;
+        public BRSHMC_MORO()
+        {
+            oDa = new DASHMC_MORO();
+        }
+        /// <summary>
+        /// OBTENER LA LISTA DE COLABORADORES
+        /// </summary>
+        /// <param name="oBe"></param>
+        /// <returns></returns>
+        public List<BESHMC_AGEN> P0007SHPR_MORO_LIST(BESHMC_MORO oBe)
+        {
+            using (var odr = oDa.P0007SHPR_MORO_LIST(oBe))
+            {
+                var oList = new List<BESHMC_AGEN>();
+                ((IList)oList).LoadFromReader<BESHMC_AGEN>(odr);
+                return (oList);
+            }
+        }
+    }
+}
