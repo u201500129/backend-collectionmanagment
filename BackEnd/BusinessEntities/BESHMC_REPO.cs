5596802106ad409f14ecf3317986207a70dd711d
Carlos Aguilar <U201521765@upc.edu.pe>
2017-09-10 22:21:40 -0500

    Se agregaron archivos de repo
    - BESHMC_REPO
    - BRSHMC_REPO
    - DASHMC_REPO
    - reporteController

---
+++ BackEnd/BusinessEntities/BESHMC_REPO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class BESHMC_REPO
+    {
+        public int COD_VISI { get; set; }
+        public int COD_REPO { get; set; }
+        public DateTime? FEC_REPO { get; set; }
+        public string ALF_REPO { get; set; }
+        public string COD_USUA_CREA { get; set; }
+        public string COD_USUA_MODI { get; set; }
+        public int NUM_ACCI { get; set; }
+    }
+}
---
+++ BackEnd/BussinessRules/BRSHMC_REPO.cs
@@ -0,0 +1,36 @@
+﻿using BusinessEntities;
+using DataAccess;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResultSetMappers;
+
+namespace BussinessRules
+{
+    public class BRSHMC_REPO
+    {
+        private DASHMC_REPO oDa;
+        public BRSHMC_REPO()
+        {
+            oDa = new DASHMC_REPO();
+        }
+        /// <summary>
+        /// AGREGAR REPORTE
+        /// </summary>
+        /// <param name="oBe"></param>
+        public void P0009SHPR_REPO(BESHMC_REPO oBe)
+        {
+            try
+            {
+                oDa.P0009SHPR_REPO(oBe);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
+            }
+        }
+    }
+}
